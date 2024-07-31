9202403c4be596164497d1d35f97fcb733a4c95e
naracci.vincenzo <143946955+naraccivincenzo@users.noreply.github.com>
2024-07-31 22:49:31 +0200

    Exercise Completed

---
+++ src/test/java/org/lessons/booleaners/hellospringmvc/HelloSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.booleaners.hellospringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
